91fa4289a976fa0094e231ae5711b713e3cb5636
Priyansh Singh <priyansh.singh211@gmail.com>
2026-01-02 21:07:27 +0530

    Week1:BuySellStock

---
+++ week1/BuySellStock.java
@@ -0,0 +1,20 @@
+public class BuySellStock {
+    public int stock(int[] prices){
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for(int price : prices){
+            if(price < min){
+                min = price;
+            }
+            else{
+                max = Math.max(max, price-min);
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        BuySellStock sol = new BuySellStock();
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(sol.stock(prices));
+    }
+}
