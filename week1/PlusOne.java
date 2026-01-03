3991e1b90d31a44e72b9d3a64f2d1df69d06bbb1
Priyansh Singh <priyansh.singh211@gmail.com>
2026-01-03 17:32:48 +0530

    Week1:PlusOne

---
+++ week1/PlusOne.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class PlusOne {
+    public int[] res(int[] nums){
+        int n = nums.length;
+
+        for(int i = n-1; i>= 0 ; i--){
+            if(nums[i]<9){
+                nums[i]++;
+                return nums;
+            }
+            nums[i] = 0;
+        }
+        
+        int[] res = new int[nums.length+1];
+        res[0] = 1;
+        return res;
+    }
+    
+    public static void main(String[] args) {
+        PlusOne sol = new PlusOne();
+        int[] arr =  {4,3,2,9 };
+        System.out.println(Arrays.toString(sol.res(arr)));
+    }
+}
