0c2cb7c439303ba3b3e0f21e6c21a81092c88fd5
Priyansh Singh <priyansh.singh211@gmail.com>
2026-01-04 15:10:52 +0530

    EvenDigitNumbers

--- week1/EvenDigitNumbers.java
+++ week1/EvenDigitNumbers.java
@@ -1,24 +1,22 @@
 public class EvenDigitNumbers {
     public int findNumbers(int[] nums){
-        int output =0;
-        for (int number: nums){
-            int count =0;
-            int temp = number;
-            while(temp != 0){
-                temp/=10;
-                count++;
+        int output = 0;
+        for (int num : nums){
+            int count = 0;
+            while(num != 0){
+            num /= 10;
+            count++;
             }
-            if (count%2 == 0){
+            if(count % 2 == 0){
                 output++;
-                }
-
+            }
         }
         return output;
     }
 
     public static void main(String[] args) {
         EvenDigitNumbers sol = new EvenDigitNumbers();
-        int[] acc = {123,77,91829,8888};
+        int[] acc = {123,745,91829,8888};
         System.out.println(sol.findNumbers(acc));
     }
 }
