a2227e0ea97eb4e0852727dfafec80cc99f568fa
Priyansh Singh <priyansh.singh211@gmail.com>
2026-01-04 17:41:23 +0530

    ReverseString

--- week1/ReverseString.java
+++ week1/ReverseString.java
@@ -2,14 +2,16 @@ import java.util.Arrays;
 
 public class ReverseString {
     public void reverse(char[] s){
-        int left = 0;
-        int right = s.length-1;
-        while(left < right){
-            char temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
-            left++;
-            right--;
+        int n = s.length;
+        int l = 0;
+        int r = n-1;
+
+        while(l<r){
+            char temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+            l++;
+            r--;
         }
         
     }
