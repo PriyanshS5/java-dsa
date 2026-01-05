e5bec3a7237437c46338480f0cd459dbaa815cbd
Priyansh Singh <priyansh.singh211@gmail.com>
2026-01-05 19:33:37 +0530

    RemoveElement

---
+++ week1/RemoveElement.java
@@ -0,0 +1,25 @@
+public class RemoveElement {
+    public int ele(int[] nums, int val){
+        int k = 0;
+        int pos = 0; 
+        for(int i = 0; i< nums.length; i++){
+            if(nums[i] != val){
+                nums[pos] = nums[i];
+                pos++;
+                k++;
+            }
+        }
+        while(pos<nums.length){
+            nums[pos] = '_';
+            pos++;
+        }
+        return k;
+    }
+    public static void main(String[] args) {
+        RemoveElement sol = new RemoveElement();
+        int[] arr = {1,2,2,4,5,2};
+        int val = 2;
+        System.out.println(sol.ele(arr, val));
+        
+    }
+}
