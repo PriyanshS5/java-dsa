e432d4b941b191f715eb4626894f768e1de56893
Priyansh Singh <priyansh.singh211@gmail.com>
2026-01-22 23:37:40 +0530

    RotateArray

--- week1/RotateArray.java
+++ week1/RotateArray.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class RotateArray {
-    public void rotate(int[] nums, int  k){
+    public void rotater(int[] nums, int  k){
         int n = nums.length;
         k %= n;
 
@@ -25,7 +25,7 @@ public class RotateArray {
         RotateArray sol = new RotateArray();
         int[] nums = {1,2,3,4,5,6,7};
         int k = 3;
-        sol.rotate(nums, k);
+        sol.rotater(nums, k);
         System.out.print(Arrays.toString(nums));
     }
 }
