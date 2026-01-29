e2a10001cae008cf55ea66ecce1e203ca9fd7fa9
Priyansh Singh <priyansh.singh211@gmail.com>
2026-01-29 23:45:28 +0530

    RichestCustomerWealth

--- week1/RichestCustomerWealth.java
+++ week1/RichestCustomerWealth.java
@@ -1,5 +1,5 @@
 public class RichestCustomerWealth {
-    public int maximaWealth(int[][] accounts) {
+    public int maxWealth(int[][] accounts) {
        int maxPaisa = 0;
        for (int[] customer : accounts){
         int sum = 0;    
@@ -15,6 +15,6 @@ public class RichestCustomerWealth {
     public static void main(String []args){
         RichestCustomerWealth soll = new RichestCustomerWealth();
         int[][] acc = {{1,2,3},{8,9,3},{10,3,4,5}};
-        System.out.println(soll.maximaWealth(acc));
+        System.out.println(soll.maxWealth(acc));
     }
 }
