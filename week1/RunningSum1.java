7d7369e3aa2ff84503eb5bb18f90ce54446c35e8
Priyansh Singh <priyansh.singh211@gmail.com>
2026-02-03 23:13:15 +0530

    RunningSum1

--- week1/RunningSum1.java
+++ week1/RunningSum1.java
@@ -16,7 +16,7 @@ public class RunningSum1 {
     }
     public static void main(String[] args) {
         RunningSum1 solu = new RunningSum1();
-        int[] aura = {1,1,1,1,1};
+        int[] aura = {1,1,1,1,2};
         System.out.println(Arrays.toString(solu.summ(aura)));
     }
 }
