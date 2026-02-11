c0602b348471eb59aa4fe06d963c78be5218a051
Priyansh Singh <priyansh.singh211@gmail.com>
2026-02-11 22:48:07 +0530

    SumOfArray

--- week1/SumOfArray.java
+++ week1/SumOfArray.java
@@ -3,12 +3,12 @@ public class SumOfArray {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
 
-        int sum = 0;
+        int summ = 0;
         for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
+            summ += arr[i];
         }
 
-        System.out.println( sum);
+        System.out.println( summ);
 
         
     }
